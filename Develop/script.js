6c8ec01653e36ad4cbdcc686dc937a71c4556ea4
Kevin Tran <kevin_tran95@yahoo.com>
2021-08-21 21:30:19 -0700

    added more to javascript

--- Develop/script.js
+++ Develop/script.js
@@ -8,10 +8,62 @@ var threePm = ("#3pm");
 var fourPm = ("#4pm");
 var fivePm = ("#5pm");
 var sixPm = ("#6pm");
+var hourSpan;
+var userInput = ""
+
 
 var currentDate = moment().format("dddd") + moment().format("MMM Do YYYY");
 var currentTime = moment().format('h:mm:ss a');
-
 var hour = moment().hours();
-var userInput = ""
-var hourSpan;
+
+ function init() {
+     
+    console.log("Current Hour" + hour)
+    var init9am = JSON.parse(localStorage.getItem("9:00am"));
+    nineAm.val(init9am);
+
+    var init10am = JSON.parse(localStorage.getItem("10:00am"));
+    nineAm.val(init10am);
+
+    var init11am = JSON.parse(localStorage.getItem("11:00am"));
+    nineAm.val(init11am);
+
+    var init12pm = JSON.parse(localStorage.getItem("12:00pm"));
+    nineAm.val(init12pm);
+
+    var init1pm = JSON.parse(localStorage.getItem("1:00pm"));
+    nineAm.val(init1pm);
+
+    var init2pm = JSON.parse(localStorage.getItem("2:00pm"));
+    nineAm.val(init2pm);
+
+    var init3pm = JSON.parse(localStorage.getItem("3:00pm"));
+    nineAm.val(init3pm);
+
+    var init4pm = JSON.parse(localStorage.getItem("4:00pm"));
+    nineAm.val(init4pm);
+
+    var init5pm = JSON.parse(localStorage.getItem("5:00pm"));
+    nineAm.val(init5pm);
+
+    var init6pm = JSON.parse(localStorage.getItem("6:00pm"));
+    nineAm.val(init6pm);
+
+ }
+
+ function control () {
+      
+    $(".form-control").each(function () {
+        var test = parseInt($(this).attr("id"));
+        hour = parseInt(hour);
+       
+        if (hour > test) {
+            $(this).addClass("past");
+        } else if (hour < test) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        }
+    });
+  }
+        
